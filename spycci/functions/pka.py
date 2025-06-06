15d4de4c39b7ecd20c1aadca3e86181e67c184df
ppravatto <pravatto1994@gmail.com>
2025-06-06 09:32:33 +0200

    Early implementation of the oxonium scheme and automated workflow

--- spycci/functions/pka.py
+++ spycci/functions/pka.py
@@ -1,13 +1,18 @@
+from typing import Union, Optional
 from spycci.systems import Ensemble
 from spycci.systems import System
 from spycci.core.base import Engine
 from spycci.engines.xtb import XtbInput
+from spycci.engines.orca import OrcaInput
 from spycci.wrappers.crest import deprotonate
 from spycci.tools.reorderenergies import reorder_energies
 
-from spycci.constants import Eh_to_kcalmol, proton_hydration_free_energy
+import spycci.constants as scon
 
-import logging
+import logging, sh, shutil, os
+from tempfile import mkdtemp
+
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -54,12 +59,18 @@ def _check_structure_acid_base_pair(protonated: System, deprotonated: System) ->
         raise RuntimeError(msg)
 
 
-def _validate_acid_base_pair(protonated: System, deprotonated: System) -> bool:
+def _validate_acid_base_pair(
+    protonated: System,
+    deprotonated: System,
+    water: Optional[System] = None,
+    oxonium: Optional[System] = None,
+) -> bool:
     """
     Checks if the provided `protonated` and `deprotonated` objects are compatible with each other and
     with a pKa calculation and verify the matching between levels of theory used in the computation.
-    If validation fails an exception is raised. The function returns a bool value correspondent to the
-    availability of vibronic calculations.
+    The function also accepts a `water` and `oxonium` object as optionals to verify the compatibility
+    of the levels of theory in case of the oxonium scheme. If validation fails an exception is raised.
+    The function returns a bool value correspondent to the availability of vibronic calculations.
 
     Parameters
     ----------
@@ -67,6 +78,10 @@ def _validate_acid_base_pair(protonated: System, deprotonated: System) -> bool:
         The molecule in the protonated form
     deprotonated : System object
         The molecule in the deprotonated form
+    water : Optional[System]
+        The water molecule to be used in the calculation
+    oxonium : Optional[System]
+        The oxonium molecule to be used in the calculation
 
     Raises
     ------
@@ -80,6 +95,14 @@ def _validate_acid_base_pair(protonated: System, deprotonated: System) -> bool:
     bool
         True if the two system have appropriate and matching vibronic levels of theory. False otherwise.
     """
+    # Check if the user provided both water and oxonium and check the structures
+    if oxonium is None and water is None:
+        pass
+    elif None in [oxonium, water]:
+        msg = "water and oxonium molecules must be checked simultaneously."
+        logger.error(msg)
+        raise RuntimeError(msg)
+
     # Check the provided structures for type, atomcount and charge
     _check_structure_acid_base_pair(protonated, deprotonated)
 
@@ -98,38 +121,84 @@ def _validate_acid_base_pair(protonated: System, deprotonated: System) -> bool:
     elot_protonated = protonated.properties.level_of_theory_electronic
     elot_deprotonated = deprotonated.properties.level_of_theory_electronic
     if elot_protonated != elot_deprotonated:
-        print(elot_protonated)
-        print(elot_deprotonated)
         msg = "Mismatch found between electronic levels of theory."
         logger.error(msg)
         raise RuntimeError(msg)
 
-    # Check if both speces have matching vibronic energy values associated
-    if (
-        protonated.properties.vibronic_energy is not None
-        and protonated.properties.level_of_theory_vibronic
-        == deprotonated.properties.level_of_theory_vibronic
-    ):
+    # Check, if povided, the water and oxonium molecules
+    if oxonium and water:
 
-        if elot_protonated != protonated.properties.level_of_theory_vibronic:
-            logger.warning(
-                f"Vibronic level of theory ({protonated.properties.level_of_theory_vibronic}) is different form the electronic one ({elot_protonated}). The pKa calculation will be executed anyway."
-            )
+        # Check the provided structures for type, atomcount and charge
+        _check_structure_acid_base_pair(oxonium, water)
+
+        # Check that both the speces have an electronic energy value associated
+        if water.properties.electronic_energy is None:
+            msg = "Electronic energy not found for water molecule."
+            logger.error(msg)
+            raise RuntimeError(msg)
+
+        if oxonium.properties.electronic_energy is None:
+            msg = "Electronic energy not found for oxonium ion."
+            logger.error(msg)
+            raise RuntimeError(msg)
+
+        # Check if the electronic level of theory for both molecules match
+        elot_water = water.properties.level_of_theory_electronic
+        elot_oxonium = oxonium.properties.level_of_theory_electronic
+        if elot_water != elot_oxonium or elot_water != elot_protonated:
+            msg = "Mismatch found between electronic levels of theory."
+            logger.error(msg)
+            raise RuntimeError(msg)
+
+    # Check if the speces have vibronic energy values associated
+    if (
+        protonated.properties.vibronic_energy is None
+        or deprotonated.properties.vibronic_energy is None
+    ):
+        msg = "Vibronic energies not found. The pKa calculation will be executed with only electronic energies"
+        logger.warning(msg)
+        return False
+
+    if oxonium and water:
+
+        if (
+            water.properties.vibronic_energy is None
+            or oxonium.properties.vibronic_energy is None
+        ):
+            msg = "Vibronic energies not found. The pKa calculation will be executed with only electronic energies"
+            logger.warning(msg)
+            return False
+
+    # Check if the level
+    vlot_protonated = protonated.properties.level_of_theory_vibronic
+    vlot_deprotonated = deprotonated.properties.level_of_theory_vibronic
+    if vlot_protonated == vlot_deprotonated:
+
+        if oxonium and water:
+            vlot_water = water.properties.level_of_theory_vibronic
+            vlot_oxonium = oxonium.properties.level_of_theory_vibronic
+            if vlot_protonated != vlot_water or vlot_protonated != vlot_oxonium:
+                msg = "Vibronic energies not found. The pKa calculation will be executed with only electronic energies"
+                logger.warning(msg)
+                return False
+
+        if elot_protonated != vlot_protonated:
+            msg = f"Vibronic level of theory ({vlot_protonated}) is different form the electronic one ({elot_protonated}). The pKa calculation will be executed anyway."
+            logger.warning(msg)
 
         return True
 
     else:
-        logger.warning(
-            "Vibronic energies not found. The pKa calculation will be executed with only electronic energies"
-        )
-
+        msg = "Vibronic energies not found. The pKa calculation will be executed with only electronic energies"
+        logger.warning(msg)
         return False
 
 
 def calculate_pka(protonated: System, deprotonated: System):
     """
-    Calculates the pKa of a molecule, given the protonated and deprotonated forms assuming
-    that energies have already been computed.
+    Calculates the pKa of a molecule using the direct method. The function expects as arguments
+    the protonated and deprotonated forms of the molecule as a `System` objects for which the
+    energies must have already been computed.
 
     Parameters
     ----------
@@ -143,18 +212,19 @@ def calculate_pka(protonated: System, deprotonated: System):
     pKa : float
         pKa of the molecule.
     """
-
     # Validate systems and check if vibronic energies are available
     with_vibronic = _validate_acid_base_pair(protonated, deprotonated)
 
     # Extract electronic energies
-    protonated_energy = protonated.properties.electronic_energy * Eh_to_kcalmol
-    deprotonated_energy = deprotonated.properties.electronic_energy * Eh_to_kcalmol
+    protonated_energy = protonated.properties.electronic_energy * scon.Eh_to_kcalmol
+    deprotonated_energy = deprotonated.properties.electronic_energy * scon.Eh_to_kcalmol
 
     # If available consider vibronic energies
     if with_vibronic:
-        protonated_energy += protonated.properties.vibronic_energy * Eh_to_kcalmol
-        deprotonated_energy += deprotonated.properties.vibronic_energy * Eh_to_kcalmol
+        protonated_energy += protonated.properties.vibronic_energy * scon.Eh_to_kcalmol
+        deprotonated_energy += (
+            deprotonated.properties.vibronic_energy * scon.Eh_to_kcalmol
+        )
 
     # If gfn2 from xTB is used consider an additional correction factor for the proton self energy
     proton_self_energy = 0
@@ -165,10 +235,10 @@ def calculate_pka(protonated: System, deprotonated: System):
     pka = (
         (
             deprotonated_energy
-            + (proton_hydration_free_energy + proton_self_energy)
+            + (scon.proton_hydration_free_energy + proton_self_energy)
             - protonated_energy
         )
-    ) / (2.303 * 1.98720425864083 / 1000 * 298.15)
+    ) / (np.log(10.0) * scon.R * 298.15 / scon.kcal_to_J)
 
     protonated.properties.set_pka(
         value=pka,
@@ -177,6 +247,137 @@ def calculate_pka(protonated: System, deprotonated: System):
     )
 
     return pka
+
+
+def calculate_pka_oxonium_scheme(
+    protonated: System, deprotonated: System, water: System, oxonium: System
+):
+    """
+    Calculates the pKa of a molecule using the oxonium method. The function expects as arguments
+    the protonated and deprotonated forms of the molecule as a `System` objects for which the
+    energies must have already been computed.
+
+    Parameters
+    ----------
+    protonated : System object
+        molecule in the protonated form
+    deprotonated : System object
+        molecule in the deprotonated form
+
+    Returns
+    -------
+    pKa : float
+        pKa of the molecule.
+    """
+    # Validate systems and check if vibronic energies are available
+    with_vibronic = _validate_acid_base_pair(protonated, deprotonated)
+
+    # Extract electronic energies
+    protonated_energy = protonated.properties.electronic_energy * scon.Eh_to_kcalmol
+    deprotonated_energy = deprotonated.properties.electronic_energy * scon.Eh_to_kcalmol
+    water_energy = water.properties.electronic_energy * scon.Eh_to_kcalmol
+    oxonium_energy = oxonium.properties.electronic_energy * scon.Eh_to_kcalmol
+
+    # If available consider vibronic energies
+    if with_vibronic:
+        protonated_energy += protonated.properties.vibronic_energy * scon.Eh_to_kcalmol
+        deprotonated_energy += (
+            deprotonated.properties.vibronic_energy * scon.Eh_to_kcalmol
+        )
+        water_energy += water.properties.vibronic_energy * scon.Eh_to_kcalmol
+        oxonium_energy += oxonium.properties.vibronic_energy * scon.Eh_to_kcalmol
+
+    # Compute the pKa and set it as a property of the protonated molecule
+    pka = (
+        (deprotonated_energy + oxonium_energy - protonated_energy - water_energy)
+    ) / (np.log(10.0) * scon.R * 298.15 / scon.kcal_to_J) - np.log10(997.0 / 18.01528)
+
+    protonated.properties.set_pka(
+        value=pka,
+        electronic_engine=protonated.properties.level_of_theory_electronic,
+        vibronic_engine=protonated.properties.level_of_theory_vibronic,
+    )
+
+    return pka
+
+
+def run_pKa_workflow(
+    protonated: System,
+    deprotonated: System,
+    method_geometry: Union[XtbInput, OrcaInput],
+    method_energy: Union[XtbInput, OrcaInput],
+    ncores: int = None,
+    maxcore: int = 350,
+) -> dict:
+    # Check if the given structures are compatible with a pKa calculation
+    _check_structure_acid_base_pair(protonated, deprotonated)
+
+    # Check if the method to be used for geometry optimization is xtb or orca
+    if type(method_geometry) not in [XtbInput, OrcaInput]:
+        raise TypeError("The geometry optimization method must be xTB or orca.")
+
+    # Check if the method to be used for energy calculations is xtb or orca
+    if type(method_energy) not in [XtbInput, OrcaInput]:
+        raise TypeError("The geometry optimization method must be xTB or orca.")
+
+    # Define structures of water and oxonium ion
+    WATER = [
+        ["O", -5.02534, 1.26595, 0.01097],
+        ["H", -4.05210, 1.22164, -0.01263],
+        ["H", -5.30240, 0.44124, -0.42809],
+    ]
+
+    OXONIUM = [
+        ["O", -7.37112, 1.56763, 0.10145],
+        ["H", -6.40989, 1.39069, -0.03899],
+        ["H", -7.67217, 2.18471, -0.60766],
+        ["H", -7.85266, 0.71353, -0.01396],
+    ]
+
+    # Write temporary files with the coordinates for water and oxonium and create
+    # the corresponding systems
+    tdir = mkdtemp(prefix="pka_workflow_", suffix=f"_tmp", dir=os.getcwd())
+
+    with sh.pushd(tdir):
+        with open("water.xyz", "w") as xyzfile:
+            xyzfile.write(f"{len(WATER)}\n\n")
+            for line in WATER:
+                xyzfile.write("    ".join(str(x) for x in line) + "\n")
+
+        with open("oxonium.xyz", "w") as xyzfile:
+            xyzfile.write(f"{len(OXONIUM)}\n\n")
+            for line in OXONIUM:
+                xyzfile.write("    ".join(str(x) for x in line) + "\n")
+
+        water = System("water.xyz", charge=0, spin=1)
+        oxonium = System("oxonium.xyz", charge=1, spin=1)
+
+    shutil.rmtree(tdir)
+
+    # Run geometry optimization for all structures in solvent
+    protonated_opt = method_geometry.opt(protonated, ncores=ncores, maxcore=maxcore)
+    deprotonated_opt = method_geometry.opt(deprotonated, ncores=ncores, maxcore=maxcore)
+    water_opt = method_geometry.opt(water, ncores=ncores, maxcore=maxcore)
+    oxonium_opt = method_geometry.opt(oxonium, ncores=ncores, maxcore=maxcore)
+
+    # Run frequency calculation for all structures in solvent
+    if method_energy != method_geometry:
+        method_energy.freq(protonated_opt, ncores=ncores, maxcore=maxcore, inplace=True)
+        method_energy.freq(
+            deprotonated_opt, ncores=ncores, maxcore=maxcore, inplace=True
+        )
+        method_energy.freq(water_opt, ncores=ncores, maxcore=maxcore, inplace=True)
+        method_energy.freq(oxonium_opt, ncores=ncores, maxcore=maxcore, inplace=True)
+
+    # Calculate the pKa using the direct method
+    pKa_direct = calculate_pka(protonated_opt, deprotonated_opt)
+
+    # Run geometry optimizazion of water an oxonium ion
+    pKa_oxonium = calculate_pka_oxonium_scheme(
+        protonated_opt, deprotonated_opt, water_opt, oxonium_opt
+    )
+
+    return pKa_direct, pKa_oxonium
 
 
 def auto_calculate_pka(
