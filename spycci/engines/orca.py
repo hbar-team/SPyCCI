aa6a3cfbc80f598bc856a601722aacf7a8f0aeec
Luca Babetto <l.babetto@cineca.it>
2025-09-09 16:14:12 +0200

    Fix ORCADIR behaviour

--- spycci/engines/orca.py
+++ spycci/engines/orca.py
@@ -539,7 +539,7 @@ class OrcaInput(Engine):
         self.solvent = solvent
         self.optionals = optionals
         self.blocks = blocks if blocks else {}
-        self.__ORCADIR = ORCADIR if ORCADIR else locate_orca().rstrip("/orca")
+        self.__ORCADIR = locate_orca(ORCADIR if ORCADIR else "orca").rstrip("/orca")
 
         self.level_of_theory += f""" | basis: {basis_set} | solvent: {solvent}"""
 
