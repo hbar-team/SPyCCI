20bc3efe13e31374150cd16ab1b1726d75733056
ppravatto <pravatto1994@gmail.com>
2025-09-12 20:32:03 +0200

    Code refactoring using common helper functions

--- spycci/tools/vmdtools.py
+++ spycci/tools/vmdtools.py
@@ -55,26 +55,26 @@ class VMDRenderer:
         VMD_PATH: Optional[str] = None,
     ) -> None:
 
+        # Check and set the user provided scale
         if scale <= 0.0:
-            raise ValueError(
-                "The VMD frame scale factor must be a non-zero positive float."
-            )
+            raise ValueError("Frame scale factor must be a non-zero positive float.")
         self.__scale: float = scale
 
+        # Check and set the user provided frame orientation
         if len(xyx_rotation) != 3:
             raise ValueError("The XYX rotation must be a list of 3 float values.")
         self.__xyx_rotation: float = [float(v) for v in xyx_rotation]
 
+        # Store basic rendering settings
         self.resolution: int = resolution
         self.shadows: bool = shadows
         self.ambientocclusion: bool = ambientocclusion
         self.dof: bool = dof
 
+        # Search for the VMD folder and set the vmd root variable
         self.__vmd_root = None
         if VMD_PATH and isfile(VMD_PATH) is False:
-            raise FileNotFoundError(
-                f"The VMD_PATH {VMD_PATH} does not point to a valid vmd executable."
-            )
+            raise FileNotFoundError(f"The VMD_PATH {VMD_PATH} does not point to a valid executable.")
 
         elif VMD_PATH:
             self.__vmd_root = locate_vmd(VMD_PATH).rstrip("/bin/vmd")
@@ -82,6 +82,7 @@ class VMDRenderer:
         else:
             self.__vmd_root = locate_vmd().rstrip("/bin/vmd")
 
+        # Check the availability of the tachyon ray tracer
         self.__tachyon_path = join(self.__vmd_root, "lib/vmd/tachyon_LINUXAMD64")
         if isfile(self.__tachyon_path) is False:
             raise RuntimeError(
@@ -128,7 +129,6 @@ class VMDRenderer:
             raise ValueError("The XYX rotation must be a list of 3 float values.")
         self.__xyx_rotation: float = [float(v) for v in value]
 
-
     def render_molecule(self, molecule_file: str) -> None:
         """
         Given the path to a molecule file (e.g. .xyz, .pdb) the function saves a `.bmp` render
@@ -144,22 +144,15 @@ class VMDRenderer:
             raise FileNotFoundError(f"Unable to find the {molecule_file} file.")
 
         root_name = basename(molecule_file).rsplit(".", 1)[0]
-        script = self._header()
+        script = self._tcl_script_preamble()
 
         # Load the molecule from file and plot its backbone
         script += f"mol new {molecule_file}\n"
-        script += "mol addrep 0\n"
-        script += "mol modstyle 0 0 CPK 1.000000 0.300000 150.000000 12.000000\n"
-        script += "mol color Name\n"
-        script += "mol selection all\n"
-        script += "mol material Opaque\n"
-        script += "mol modcolor 0 0 Element\n"
+        script += self._tcl_plot_backbone()
 
         self._render(script, root_name)
 
-    def render_fukui_cube(
-        self, cubfile: str, isovalue: float = 0.003, include_negative: bool = False
-    ) -> None:
+    def render_fukui_cube(self, cubefile: str, isovalue: float = 0.003, show_negative: bool = False) -> None:
         """
         Given the path to a Fukui function cube file saves a `.bmp` render the volumetric Fukui
         function.
@@ -170,46 +163,25 @@ class VMDRenderer:
             The path to the `.fukui.cube` file that must be rendered.
         isovalue: float
             The isovalue at which the contour must be plotted (default: 0.003).
-        include_negative: bool
+        show_negative: bool
             If set to True, will render also the negative part of the Fukui function. (default:
             False)
         """
-        # Check if given file exists
-        if isfile(cubfile) is False:
-            raise FileNotFoundError(f"Unable to find the {cubfile} file.")
+        root_name = basename(cubefile).rstrip(".fukui.cube")
 
-        root_name = basename(cubfile).rstrip(".fukui.cube")
-        script = self._header()
+        script = self._tcl_script_preamble()
 
-        # Plot the molecular backbone
-        script += f"mol new {cubfile} type {{cube}} first 0 last -1 step 1 waitfor 1 volsets {{0 }}\n"
-        script += "mol modstyle 0 0 CPK 1.000000 0.300000 150.000000 12.000000\n"
-        script += "mol color Name\n"
-        script += "mol addrep 0\n"
+        script += self._tcl_cube_script(
+            cubefile,
+            isovalue=isovalue,
+            positive_color=1,
+            negative_color=0,
+            show_negative=show_negative,
+        )
 
-        # Print positive part of the isosurface
-        script += "mol modcolor 1 0 ColorID 1\n"
-        script += f"mol modstyle 1 0 Isosurface {isovalue} 0 0 0 1 1\n"
-        script += "mol modmaterial 1 0 Translucent\n"
-        script += "mol scaleminmax 0 1 0.000000 1.000000\n"
-        script += "display cuemode Linear\n"
-
-        # If required print the negative part of the isosurface
-        if include_negative:
-            script += "mol addrep 0\n"
-            script += "mol modcolor 2 0 ColorID 0\n"
-            script += "mol modstyle 2 0 Isosurface {-isovalue} 0 0 0 1 1\n"
-            script += "mol modmaterial 2 0 Translucent\n"
-            script += "mol scaleminmax 0 2 -1.000000 0.000000\n"
-        
-        # Apply some final settings
-        script += "display cuemode Linear"
-        script += "mol selection all"
-        script += "mol material Opaque"
-                
         self._render(script, root_name)
 
-    def render_condensed_fukui(self, cubfile: str) -> None:
+    def render_condensed_fukui(self, cubefile: str) -> None:
         """
         Given the path to a Fukui function cube file saves a `.bmp` render of the condensed Fukui
         functions.
@@ -220,15 +192,15 @@ class VMDRenderer:
             The path to the `.fukui.cube` file that must be rendered.
         """
         # Check if given file exists
-        if isfile(cubfile) is False:
-            raise FileNotFoundError(f"Unable to find the {cubfile} file.")
+        if isfile(cubefile) is False:
+            raise FileNotFoundError(f"Unable to find the {cubefile} file.")
 
-        root_name = basename(cubfile).rstrip(".fukui.cube")
-        script = self._header()
+        root_name = basename(cubefile).rstrip(".fukui.cube")
+        script = self._tcl_script_preamble()
 
         # Load the molecule from the cube file, plot its backbone using Licorice style
         # and color it using the data encoding the condensed Fukui values (saved as partial charges)
-        script += f"mol new {cubfile} type {{cube}} first 0 last -1 step 1 waitfor 1 volsets {{0 }}\n"
+        script += f"mol new {cubefile} type {{cube}} first 0 last -1 step 1 waitfor 1 volsets {{0 }}\n"
         script += "mol addrep 0\n"
         script += "mol modstyle 0 0 Licorice 0.1 20.000000 20.000000\n"
         script += "color scale method BWR\n"
@@ -253,10 +225,10 @@ class VMDRenderer:
         script += "display cuemode Linear"
         script += "mol selection all"
         script += "mol material Opaque"
-            
+
         self._render(script, root_name)
 
-    def render_spin_density_cube(self, cubfile: str, isovalue: float = 0.005) -> None:
+    def render_spin_density_cube(self, cubefile: str, isovalue: float = 0.005) -> None:
         """
         Given the path to a spin density cube file saves a `.bmp` render the function.
 
@@ -270,35 +242,17 @@ class VMDRenderer:
             The tuple of 3 rotation angles (from 0 to 360°) defyning subsequent rotations around
             the X, Y and X axis. If None (default), no rotation is applied.
         """
-        # Check if given file exists
-        if isfile(cubfile) is False:
-            raise FileNotFoundError(f"Unable to find the {cubfile} file.")
+        root_name = basename(cubefile).rstrip(".spindens.cube")
 
-        root_name = basename(cubfile).rstrip(".spindens.cube")
-        script = self._header()
+        script = self._tcl_script_preamble()
 
-        # Plot the molecular backbone
-        script += f"mol new {cubfile} type {{cube}} first 0 last -1 step 1 waitfor 1 volsets {{0 }}\n"
-        script += "mol modstyle 0 0 CPK 1.000000 0.300000 150.000000 12.000000\n"
-        script += "mol color Name\n"
-        script += "mol addrep 0\n"
-        
-        # Print positive part of the isosurface
-        script += "mol modcolor 1 0 ColorID 31\n"
-        script += f"mol modstyle 1 0 Isosurface {isovalue} 0 0 0 1 1\n"
-        script += "mol modmaterial 1 0 Translucent\n"
-        script += "mol scaleminmax 0 1 0.000000 1.000000\n"
-
-        # Print negative part of the isosurface
-        script += "mol modcolor 2 0 ColorID 26\n"
-        script += "mol modstyle 2 0 Isosurface {-isovalue} 0 0 0 1 1\n"
-        script += "mol modmaterial 2 0 Translucent\n"
-        script += "mol scaleminmax 0 2 -1.000000 0.000000\n"
-
-        # Apply some final settings
-        script += "display cuemode Linear\n"
-        script += "mol selection all\n"
-        script += "mol material Opaque\n"
+        script += self._tcl_cube_script(
+            cubefile,
+            isovalue=isovalue,
+            positive_color=31,
+            negative_color=26,
+            show_negative=True,
+        )
 
         self._render(script, root_name)
 
@@ -340,18 +294,18 @@ class VMDRenderer:
                 vmd_script.write("display dof on\n")
 
             vmd_script.write(
-                f"""
-                render Tachyon {output_name}.dat "{self.__tachyon_path}" -fullshade -aasamples 12 %s -format BMP -res {self.resolution} {self.resolution} -o {output_name}.bmp
-                exit
-                """
+                f"""render Tachyon {output_name}.dat "{self.__tachyon_path}" -fullshade -aasamples 12 %s -format BMP -res {self.resolution} {self.resolution} -o {output_name}.bmp\n"""
             )
+            vmd_script.write("exit\n")
 
             vmd_script.seek(0)
             system(f"vmd -dispdev text -e {vmd_script.name}")
-    
-    def _header(self) -> str:
+
+    def _tcl_script_preamble(self) -> str:
         """
-        Generates a standar header for the VMD instructions script.
+        Generates a standard header for the VMD instructions script. The header
+        sets orthographic projection, removes axes and sets white background.
+        The preamble also sets the carbon color to black.
 
         Returns
         -------
@@ -364,4 +318,84 @@ class VMDRenderer:
         script += "axes location Off\n"
         script += "color Display Background white\n"
         script += "color Name C black\n"
+        return script
+
+    def _tcl_plot_backbone(self) -> str:
+        """
+        Generates the code required to plot the molecular backbone of a loaded mol object.
+
+        Returns
+        -------
+        str
+            The script encoding the backbone rendering.
+        """
+        script = ""
+        script += "mol selection all\n"
+        script += "mol addrep 0\n"
+        script += "mol modstyle 0 0 CPK 1.000000 0.300000 150.000000 12.000000\n"
+        script += "mol material Opaque\n"
+        script += "mol color Name\n"
+        return script
+
+    def _tcl_cube_script(
+        self,
+        cubefile: str,
+        isovalue: float = 0.01,
+        positive_color: int = 1,
+        negative_color: int = 0,
+        show_negative: bool = True,
+    ) -> str:
+        """
+        Generates a general script to render a cube file. The user can select the isovalue,
+        the color of each phase of the cube file and whether the negative phase is shown.
+
+        Arguments
+        ---------
+        cubefile: str
+            The path to the `.cube` file that must be rendered.
+        isovalue: float
+            The isovalue at which the contour must be plotted (default: 0.01).
+        positive_color: int
+            The color of the positive phase of the plot.
+        negative_color: int
+            The color of the positive phase of the plot.
+        show_negative: bool
+            If set to True will plot the negative part of the cube file.
+
+        Raises
+        ------
+        FileNotFoundError
+            Exception raised if the cube file cannot be found.
+
+        Return
+        ------
+        str
+            The script encoding the cube rendering
+        """
+        # Check if given file exists
+        if isfile(cubefile) is False:
+            raise FileNotFoundError(f"Unable to find the {cubefile} file.")
+
+        script = ""
+
+        # Plot the molecular backbone
+        script += f"mol new {cubefile} type {{cube}} first 0 last -1 step 1 waitfor 1 volsets {{0 }}\n"
+        script += self._tcl_plot_backbone()
+
+        # Print positive part of the isosurface
+        script += f"mol modcolor 1 0 ColorID {positive_color}\n"
+        script += f"mol modstyle 1 0 Isosurface {isovalue} 0 0 0 1 1\n"
+        script += "mol modmaterial 1 0 Translucent\n"
+        script += "mol scaleminmax 0 1 0.000000 1.000000\n"
+
+        # Print negative part of the isosurface
+        if show_negative is True:
+            script += f"mol modcolor 2 0 ColorID {negative_color}\n"
+            script += f"mol modstyle 2 0 Isosurface {-isovalue} 0 0 0 1 1\n"
+            script += "mol modmaterial 2 0 Translucent\n"
+            script += "mol scaleminmax 0 2 -1.000000 0.000000\n"
+
+        # Apply some final settings
+        script += "display cuemode Linear\n"
+
         return script
