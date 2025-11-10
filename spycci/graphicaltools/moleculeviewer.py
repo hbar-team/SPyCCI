c2656a4b083328aad5d51a646ead97d2f8788629
ppravatto <pravatto1994@gmail.com>
2025-11-10 20:15:24 +0100

    Added cube plotting option to Internal viewer

--- spycci/graphicaltools/moleculeviewer.py
+++ spycci/graphicaltools/moleculeviewer.py
@@ -5,6 +5,7 @@ from typing import Union, Optional, List, Tuple
 
 from spycci.core.geometry import MolecularGeometry
 from spycci.systems import System
+from spycci.tools.cubetools import Cube
 
 # Covalent radius (in Å) for single bonds from "Pekka Pyykkö and Michiko Atsumi. Molecular Single-Bond Covalent Radii for Elements 1-118. Chemistry - A European Journal, 15(1):186–197, jan 2009"
 COVALENT_RADII = {'H': 0.32, 'He': 0.46, 'Li': 1.33, 'Be': 1.02, 'B': 0.85, 'C': 0.75, 'N': 0.71, 'O': 0.63, 'F': 0.64, 'Ne': 0.67, 'Na': 1.55, 'Mg': 1.39, 'Al': 1.26, 'Si': 1.16, 'P': 1.11, 'S': 1.03, 'Cl': 0.99, 'Ar': 0.96, 'K': 1.96, 'Ca': 1.71, 'Sc': 1.48, 'Ti': 1.36, 'V': 1.34, 'Cr': 1.22, 'Mn': 1.19, 'Fe': 1.16, 'Co': 1.11, 'Ni': 1.1, 'Cu': 1.12, 'Zn': 1.18, 'Ga': 1.24, 'Ge': 1.21, 'As': 1.21, 'Se': 1.16, 'Br': 1.14, 'Kr': 1.17, 'Rb': 2.1, 'Sr': 1.85, 'Y': 1.63, 'Zr': 1.54, 'Nb': 1.47, 'Mo': 1.38, 'Tc': 1.28, 'Ru': 1.25, 'Rh': 1.25, 'Pd': 1.2, 'Ag': 1.28, 'Cd': 1.36, 'In': 1.42, 'Sn': 1.4, 'Sb': 1.4, 'Te': 1.36, 'I': 1.33, 'Xe': 1.31, 'Cs': 2.32, 'Ba': 1.96, 'La': 1.8, 'Ce': 1.63, 'Pr': 1.76, 'Nd': 1.74, 'Pm': 1.73, 'Sm': 1.72, 'Eu': 1.68, 'Gd': 1.69, 'Tb': 1.68, 'Dy': 1.67, 'Ho': 1.66, 'Er': 1.65, 'Tm': 1.64, 'Yb': 1.7, 'Lu': 1.62, 'Hf': 1.52, 'Ta': 1.46, 'W': 1.37, 'Re': 1.31, 'Os': 1.29, 'Ir': 1.22, 'Pt': 1.23, 'Au': 1.24, 'Hg': 1.33, 'Tl': 1.44, 'Pb': 1.44, 'Bi': 1.51, 'Po': 1.45, 'At': 1.47, 'Rn': 1.42, 'Fr': 2.23, 'Ra': 2.01, 'Ac': 1.86, 'Th': 1.75, 'Pa': 1.69, 'U': 1.7, 'Np': 1.71, 'Pu': 1.72, 'Am': 1.66, 'Cm': 1.66, 'Bk': 1.68, 'Cf': 1.68, 'Es': 1.65, 'Fm': 1.67, 'Md': 1.73, 'No': 1.76, 'Lr': 1.61, 'Rf': 1.57, 'Db': 1.49, 'Sg': 1.43, 'Bh': 1.41, 'Hs': 1.34, 'Mt': 1.29, 'Ds': 1.28, 'Rg': 1.21, 'Cn': 1.22, 'Nh': 1.36, 'Fl': 1.43, 'Mc': 1.62, 'Lv': 1.75, 'Ts': 1.65, 'Og': 1.57}
@@ -12,9 +13,14 @@ COVALENT_RADII = {'H': 0.32, 'He': 0.46, 'Li': 1.33, 'Be': 1.02, 'B': 0.85, 'C':
 # Atom colors derived from the JMOL standard (carbons have been set to darker grey value)
 ATOM_COLORS = {"H": "#FFFFFF", "He": "#D9FFFF", "Li": "#CC80FF", "Be": "#C2FF00", "B": "#FFB5B5", "C": "#282828", "N": "#3050F8", "O": "#FF0D0D", "F": "#90E050", "Ne": "#B3E3F5", "Na": "#AB5CF2", "Mg": "#8AFF00", "Al": "#BFA6A6", "Si": "#F0C8A0", "P": "#FF8000", "S": "#FFFF30", "Cl": "#1FF01F", "Ar": "#80D1E3", "K": "#8F40D4", "Ca": "#3DFF00", "Sc": "#E6E6E6", "Ti": "#BFC2C7", "V": "#A6A6AB", "Cr": "#8A99C7", "Mn": "#9C7AC7", "Fe": "#E06633", "Co": "#F090A0", "Ni": "#50D050", "Cu": "#C88033", "Zn": "#7D80B0", "Ga": "#C28F8F", "Ge": "#668F8F", "As": "#BD80E3", "Se": "#FFA100", "Br": "#A62929", "Kr": "#5CB8D1", "Rb": "#702EB0", "Sr": "#00FF00", "Y": "#94FFFF", "Zr": "#94E0E0", "Nb": "#73C2C9", "Mo": "#54B5B5", "Tc": "#3B9E9E", "Ru": "#248F8F", "Rh": "#0A7D8C", "Pd": "#006985", "Ag": "#C0C0C0", "Cd": "#FFD98F", "In": "#A67573", "Sn": "#668080", "Sb": "#9E63B5", "Te": "#D47A00", "I": "#940094", "Xe": "#429EB0", "Cs": "#57178F", "Ba": "#00C900", "La": "#70D4FF", "Ce": "#FFFFC7", "Pr": "#D9FFC7", "Nd": "#C7FFC7", "Pm": "#A3FFC7", "Sm": "#8FFFC7", "Eu": "#61FFC7", "Gd": "#45FFC7", "Tb": "#30FFC7", "Dy": "#1FFFC7", "Ho": "#00FF9C", "Er": "#00E675", "Tm": "#00D452", "Yb": "#00BF38", "Lu": "#00AB24", "Hf": "#4DC2FF", "Ta": "#4DA6FF", "W": "#2194D6", "Re": "#267DAB", "Os": "#266696", "Ir": "#175487", "Pt": "#D0D0E0", "Au": "#FFD123", "Hg": "#B8B8D0", "Tl": "#A6544D", "Pb": "#575961", "Bi": "#9E4FB5", "Po": "#AB5C00", "At": "#754F45", "Rn": "#428296", "Fr": "#420066", "Ra": "#007D00", "Ac": "#70ABFA", "Th": "#00BAFF", "Pa": "#00A1FF", "U": "#008FFF", "Np": "#0080FF", "Pu": "#006BFF", "Am": "#545CF2", "Cm": "#785CE3", "Bk": "#8A4FE3", "Cf": "#A136D4", "Es": "#B31FD4", "Fm": "#B31FBA", "Md": "#B30DA6", "No": "#BD0D87", "Lr": "#C70066", "Rf": "#CC0059", "Db": "#D1004F", "Sg": "#D90045", "Bh": "#E00038", "Hs": "#E6002E", "Mt": "#EB0026", "Ds": "#EB0026", "Rg": "#E6002E", "Cn": "#E00038", "Nh": "#D90045", "Fl": "#D1004F", "Mc": "#CC0059", "Lv": "#C70066", "Ts": "#BD0D87", "Og": "#B30DA6"}
 
+# Default CUBE colors
+CUBE_POSITIVE_COLOR = "#FF6600"
+CUBE_NEGATIVE_COLOR = "#0238CF"
 
 def show_molecule(
         molecule: System,
+        cube: Optional[Cube] = None,
+        isovalue: Optional[float] = None,
         atoms_colors: Optional[Union[List[str], List[Tuple[float, float, float]]]] = None,
         atom_scale : float = 0.4,
         bond_radius : float = 0.075,
@@ -41,6 +47,12 @@ def show_molecule(
     ----------
     molecule : System
         The molecular system to visualize.
+    cube: Optional[Cube]
+        The `Cube` object encoding the volumetric data to visualized. If `None` (default)
+        the volumetric data rendering will be skipped.
+    isovalue: Optional[float]
+        The isovalue at which the contour must be plotted. If set to `None` (default)
+        a proper isovalue will be set automatically as the 20% of the maximum voxel value.
     atoms_colors : Optional[Union[List[str], List[Tuple[float, float, float]]]]
         The ordered list of HEX color values or RGB triplets to be used to color the atoms in the structure.
         If `None` (default), will use the standard JMOL coloring scheme to represent the molecule.
@@ -205,6 +217,40 @@ def show_molecule(
             else:
                 cyl = pv.Cylinder(center=center, direction=direction, radius=bond_radius, height=height, resolution=24)
                 plotter.add_mesh(cyl, color='lightgray', smooth_shading=True)
+    
+    # If specified render CUBE positive and negative isosurfaces
+    if cube is not None:
+
+        # Create coordinate grid
+        nx, ny, nz = cube.nvoxels
+        x_axis, y_axis, z_axis = cube.axes
+        origin = cube.origin
+
+        # Compute coordinate vectors
+        x = origin[0] + np.arange(nx) * x_axis[0]
+        y = origin[1] + np.arange(ny) * y_axis[1]
+        z = origin[2] + np.arange(nz) * z_axis[2]
+
+        # Create structured grid
+        grid = pv.StructuredGrid(*np.meshgrid(x, y, z, indexing="ij"))
+        grid["values"] = cube.cube.flatten(order="F")
+
+        # If not specified by the user automatically compute a guess of the isovalue
+        if isovalue is None:
+            cmax, cmin = cube.max, cube.min
+            isovalue = 0.2 * max(abs(cmax), abs(cmin))
+
+        # Extract isosurface
+        positive_surface = grid.contour([isovalue])
+        negative_surface = grid.contour([-isovalue])
+
+        # Plot positive and negative isosurfaces if they are not empty
+        if positive_surface.number_of_points > 0:
+            plotter.add_mesh(positive_surface, color=CUBE_POSITIVE_COLOR, opacity=0.8, name="positive isosurface")
+        
+        if negative_surface.number_of_points > 0:
+            plotter.add_mesh(negative_surface, color=CUBE_NEGATIVE_COLOR, opacity=0.8, name="negative isosurface")
+
 
     plotter.camera.azimuth = azimuth
     plotter.camera.elevation = elevation
